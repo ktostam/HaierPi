80f99720e6d696e2109a21826b898a04ca1635bf
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-04-30 20:42:25 +0200

    work in progress update

--- HPi/GPIO.py
+++ HPi/GPIO.py
@@ -1,17 +1,21 @@
+import os.path
 HIGH = 1
 LOW = 0
 OUT = "out"
 IN = "in"
 
 def setup(pin, action):
-    file='/sys/class/gpio/export'
-    pinfile="/sys/class/gpio/gpio"+str(pin)+"/direction"
-    file1 = open(file, 'w')
-    file1.write(str(pin))
-    file1.close()
-    file2 = open(pinfile, 'w')
-    file2.write(action)
-    file2.close()
+    if os.path.isdir("/sys/class/gpio/gpio"+str(pin)+"/"):
+        print("WARNING: Channel already in use.")
+    else:
+        file='/sys/class/gpio/export'
+        pinfile="/sys/class/gpio/gpio"+str(pin)+"/direction"
+        file1 = open(file, 'w')
+        file1.write(str(pin))
+        file1.close()
+        file2 = open(pinfile, 'w')
+        file2.write(action)
+        file2.close()
 
 def output(pin, value):
     pinfile="/sys/class/gpio/gpio"+str(pin)+"/value"
