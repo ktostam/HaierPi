b84ad24d0c58fff827a5289fe4b1d9ffeb9f4072
Jacek Brzozowski <jacek.brzozowski@adm.uw.edu.pl>
2023-06-10 11:30:29 +0200

    add 'import math'

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ import PyHaier
 import socket
 import serial
 import signal
+import math
 import json
 import time
 
-version="1.35"
+version="1.36"
 welcome="\n┌────────────────────────────────────────┐\n│              "+colored("!!!Warning!!!", "red", attrs=['bold','blink'])+colored("             │\n│      This script is experimental       │\n│                                        │\n│ Products are provided strictly \"as-is\" │\n│ without any other warranty or guaranty │\n│              of any kind.              │\n└────────────────────────────────────────┘\n","yellow", attrs=['bold'])
 config = configparser.ConfigParser()
 config.read('config.ini')
